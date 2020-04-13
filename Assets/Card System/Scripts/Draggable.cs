18a8e8d854f509bf09dddf4d8961ef0a0ecd4a51
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-04-13 14:02:43 +0800

    主檔-1

---
+++ Assets/Card System/Scripts/Draggable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler,IDragHandler,IDropHandler
+{
+
+    Transform parentToReturnTo = null;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        print("OnBeginDrag");
+        transform.SetParent(transform.parent.parent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print("OnDrag");
+        transform.position = eventData.position;
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        print("OnDrop");
+    }
+
+   
+}
---
+++ Assets/Card System/Scripts/DropZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour,IDropHandler,IPointerEnterHandler,IPointerExitHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
