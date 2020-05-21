b33bbd2257fa225fc695ceacb804ca88a3128a24
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-05-21 21:32:18 +0800

    卡牌

---
+++ Assets/Card System/Scripts/DeckManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+	public static DeckManager instance;
+
+	public List<CardData> Deck = new List<CardData>();
+}
