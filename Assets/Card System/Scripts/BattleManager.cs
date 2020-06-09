8130d76d693e26f52049003d70648330d9735d10
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-06-09 21:38:09 +0800

    抽牌

---
+++ Assets/Card System/Scripts/BattleManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BattleManager : MonoBehaviour
+{
+	/// <summary>
+	/// 實體化BattleManager
+	/// </summary>
+	public static BattleManager instance;
+
+	public int  energy;
+
+	public Text energytext;
+	[Header("手牌")]
+	public List<CardData> HandDeck = new List<CardData>();
+	[Header("手牌遊戲物件")]
+	public List<GameObject> Handgameobject = new List<GameObject>();
+	public List<CardData> DropDeck = new List<CardData>();
+	public List<GameObject> Dropgameobject = new List<GameObject>();
+	private int turn; //回合
+	private Transform hand;
+	private Transform canvas;
+
+	private void Start()
+	{
+		instance = this;
+		canvas = GameObject.Find("畫布").GetComponent<Transform>();
+		hand = GameObject.Find("手牌").GetComponent<Transform>();
+	}
+
+
+	/// <summary>
+	/// 取得手牌
+	/// </summary>
+	public IEnumerator GetCard(int count)
+	{
+		yield return null;
+		for (int i = 0; i < count; i++)
+		{
+			HandDeck.Add(DeckManager.instance.BattleDeck[0]);
+			DeckManager.instance.BattleDeck.RemoveAt(0);
+			Handgameobject.Add(DeckManager.instance.Battlegameobject[0]);
+			DeckManager.instance.Battlegameobject.RemoveAt(0);
+			yield return StartCoroutine(MoveCard());
+			
+		}
+	}
+
+	private IEnumerator MoveCard()
+	{
+		RectTransform card = Handgameobject[Handgameobject.Count - 1].GetComponent<RectTransform>();
+
+		card.SetParent(canvas);
+		card.anchorMin = Vector2.one * 0.5f;
+		card.anchorMax = Vector2.one * 0.5f;
+		card.localScale = Vector3.one * 1.5f;
+		while (card.anchoredPosition.x > 501)
+		{
+			card.anchoredPosition = Vector2.Lerp(card.anchoredPosition, new Vector2(500, 0), 0.5f * Time.deltaTime * 50);
+			yield return null;
+		}
+
+		
+
+		card.localScale = Vector3.one * 0.5f;
+		while (card.anchoredPosition.y > -450)
+		{
+			card.anchoredPosition = Vector2.Lerp(card.anchoredPosition, new Vector2(0, -451), 0.5f * Time.deltaTime * 50);
+			yield return null;
+		}
+
+		card.SetParent(hand);
+		card.gameObject.AddComponent<HandCard>();
+	}
+	public void StartBattle()
+	{
+
+		StartCoroutine(GetCard(5));
+
+	}
+}
---
+++ Assets/Card System/Scripts/DeckManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckManager : MonoBehaviour
+{
+	public static DeckManager instance;
+
+	public List<CardData> Deck = new List<CardData>();
+
+	//戰鬥牌組
+	public List<CardData> BattleDeck = new List<CardData>();
+	[Header("牌組遊戲物件")]
+	public List<GameObject> Battlegameobject = new List<GameObject>();
+
+
+
+	[Header("牌組" +	"內容")]
+	public Transform deckcontent;
+
+
+	private void Start()
+	{
+		instance = this;
+
+	}
+	/// <summary>
+	/// 初始化牌組
+	/// </summary>
+	public void InitialDeck()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			Deck.Add(GetCard.instance.cards[0]);
+		}
+		for (int i = 0; i < 5; i++)
+		{
+			Deck.Add(GetCard.instance.cards[1]);
+		}
+		BattleDeck = Deck;
+		Shuffle();
+	}
+
+
+
+	/// <summary>
+	/// 建立卡牌在洗牌區
+	/// </summary>
+	private void CreateCard()
+	{
+		for (int i = 0; i < BattleDeck.Count; i++)
+		{
+			Transform temp = Instantiate(GetCard.instance.cardobject, deckcontent).transform;
+			CardData card = Deck[i];
+			temp.Find("名稱").GetComponent<Text>().text = card.name.ToString();
+			temp.Find("描述").GetComponent<Text>().text = card.description.ToString();
+			temp.Find("消耗").GetComponent<Text>().text = card.cost.ToString();
+			temp.Find("血量").GetComponent<Text>().text = card.hp.ToString();
+			temp.Find("攻擊").GetComponent<Text>().text = card.attack.ToString();
+			//temp.Find("卡圖").GetComponent<Image>().sprite = Resources.Load<Sprite>(card.file);
+			Battlegameobject.Add(temp.gameObject);
+		}
+		
+	}
+
+
+	/// <summary>
+	/// 洗牌
+	/// </summary>
+	public void Shuffle()
+	{
+		for (int i = 0; i < BattleDeck.Count; i++)
+		{
+			CardData original = BattleDeck[i];
+			int r = Random.Range(0, BattleDeck.Count);
+			BattleDeck[i] = BattleDeck[r];
+			BattleDeck[r] = original;
+
+		}
+		CreateCard();
+	}
+
+	public void AddCard(int index)
+	{
+
+
+		//取得卡牌資訊
+		CardData card = GetCard.instance.cards[index - 1];
+		Deck.Add(card);
+
+			
+		
+
+	}
+
+	/// <summary>
+	/// 刪除卡牌
+	/// </summary>
+	public void DeleteCard(int index)
+	{
+
+		//移除卡牌
+		Deck.Remove(GetCard.instance.cards[index - 1]);
+	}
+
+	
+}
